b3389ade9a7cc9db6b72d1d6728c532c075c5e50
SornchaiTheDev <sornchaithedev@gmail.com>
2023-12-24 23:00:26 +0700

    Add : metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,33 @@
-import type { Metadata } from 'next'
+import { Metadata } from 'next'
 import { twMerge } from 'tailwind-merge'
 import { Dancing_Script, Athiti } from 'next/font/google'
 import './globals.css'
 
 const dancingScript = Dancing_Script({ subsets: ['latin'] })
-const athiti = Athiti({ subsets : ["thai"] , weight: ["200", "300", "400", "500", "600", "700"] })
+const athiti = Athiti({ subsets: ["thai"], weight: ["200", "300", "400", "500", "600", "700"] })
 
 export const metadata: Metadata = {
-  title: 'Make a wish',
-  description: 'เวลาก็จะวนมาครบ 1 ปีอีกแล้วน้า ปีนี้เราผ่านอะไรมาเยอะเลย เราทุกคนเก่งมากเย่',
+  title: "Make a wish",
+  description: "เวลาก็จะวนมาครบ 1 ปีอีกแล้วน้า ปีนี้เราผ่านอะไรมาเยอะเลย เราทุกคนเก่งมากเย่",
+  authors : {
+    name : "SornchaiTheDev",
+    url : "https://sornchaithedev.com"
+  },
+  keywords: ["อวยพรปีใหม่"],
+  // openGraph: {
+  //   title: "Make a wish",
+  //   description: "เวลาก็จะวนมาครบ 1 ปีอีกแล้วน้า ปีนี้เราผ่านอะไรมาเยอะเลย เราทุกคนเก่งมากเย่",
+  //   url: process.env.DOMAIN,
+  //   images: [
+  //     {
+  //       url: `${process.env.DOMAIN}/api/v1/og/og`,
+  //       width: 1200,
+  //       height: 630
+  //     }
+  //   ]
+  // }
 }
+
 
 export default function RootLayout({
   children,
@@ -18,7 +36,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={twMerge(dancingScript.className, athiti.className ,"bg-black")}>{children}</body>
+      <body className={twMerge(dancingScript.className, athiti.className, "bg-black")}>{children}</body>
     </html>
   )
 }
